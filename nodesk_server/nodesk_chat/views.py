a69bb883e47c7beab40a0f9971768506a027b233
Julien Gamba <jgamba@aius.u-strasbg.fr>
2014-06-11 16:44:31 +0200

    Added view to read all posted messages

--- nodesk_server/nodesk_chat/views.py
+++ nodesk_server/nodesk_chat/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
 
-# Create your views here.
+from nodesk_chat.models import Post
+
+def index(request):
+    latest_messages = Post.objects.order_by('date')
+    context = {'latest_messages': latest_messages}
+    return render(request, 'index.html', context)
