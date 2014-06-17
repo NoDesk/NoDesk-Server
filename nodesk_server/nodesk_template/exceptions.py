0fb006617025a7327aeb8ebe2f4450b2bcdb6d11
Thomas Kuntz <thomaskuntz@sharedmemory.fr>
2014-06-17 14:10:21 +0200

    add nodesk_template exception file and its first Exception (unrecognized type of a field of the template)

---
+++ nodesk_server/nodesk_template/exceptions.py
@@ -0,0 +1,7 @@
+
+class UnrecognizedFieldType(Exception) :
+    def __init__(self,field_type,field_name) :
+        self.name = field_name
+        self.type = field_type
+    def __str__(self) :
+        return "%s : Unrecongnized type for the field %s" % (self.type,self.name)
