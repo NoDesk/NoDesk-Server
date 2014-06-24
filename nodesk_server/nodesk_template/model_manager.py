a085e3ab755d4197f428b808fc1b4a0e65cab0c3
Clément Schreiner <clement@mux.me>
2014-06-24 20:27:02 +0200

    Various trivial fixes (typos and such).

--- nodesk_server/nodesk_template/model_manager.py
+++ nodesk_server/nodesk_template/model_manager.py
@@ -8,7 +8,7 @@ import nodesk_template.models
 from nodesk_template.exceptions import UnrecognizedFieldType
 from nodesk_template.constants import INDENTATION
 
-from field_types import field_types
+from field_types import field_types_dict
 
 
 
@@ -20,7 +20,7 @@ def generate_model_from_YAML(file_path) :
 
     #Hash the content of the yaml file/dump produced by PyYAML (equivalent)
 
-    template_hash = hashlib.sha256( yaml.dump(template_yaml) ).hexdigest()
+    template_hash = hashlib.sha256(yaml.dump(template_yaml)).hexdigest()
 
     #Get the name of the yaml file (with extension) from the path of the file
     filename = os.path.basename(file_path)
@@ -41,40 +41,40 @@ def generate_model_from_YAML(file_path) :
         #        yaml=template_yaml.dump())
         #template.save()
 
-        model_path = '{1}/{2}'.format(nodesk_template.models.path[0], filename)
+        model_path = '{0}/{1}'.format(nodesk_template.models.__path__[0],
+                                      filename)
         with open(model_path, 'w') as model_file:
             model_file.write(get_header(filename))
 
             for field in template_yaml:
                 if (field['type'] == 'Section'):
                     for field_ in field['value']:
-                        func = field_types.get(field['type'], None)
+                        func = field_types_dict.get(field['type'], None)
                         if func is None:
-                            raise UnrecognizedFieldType(field['type'], field['name'])
+                            raise UnrecognizedFieldType(field_['value'])
 
-                        model_file.write(func(field_['value']))
+                        model_file.write(func(field_['name'], field_['value']))
 
                 else:
-                    func = field_types.get(field['type'], None)
+                    func = field_types_dict.get(field['type'], None)
                     if func is None:
-                        raise UnrecognizedFieldType(field['type'], field['name'])
+                        raise UnrecognizedFieldType(field['type'])
 
-                    model_file.write(func(field['value']))
+                    model_file.write(func(field['name'], field['value']))
 
-                model_file.write(get_meta_class)
+            model_file.write(get_meta_class())
         return template
 
 
 def get_header(class_name):
     return """from django.db import models
 
-class {1}(models.Model):
+class {0}(models.Model):
 """.format(class_name)
 
-def get_meta_class(file) :
-    file.write('\n'*4)
-    file.write(INDENTATION + "class Meta:\n")
-    file.write(INDENTATION*2 + "\"\"\"Meta Class for your model.\"\"\"\n")
-    file.write(INDENTATION*2 + "app_label = '%s'\n" % APP_NAME)
+def get_meta_class():
+    return '\n'*2 + INDENTATION + "class Meta:\n" + INDENTATION*2 + \
+        "\"\"\"Meta Class for your model.\"\"\"\n" + \
+        INDENTATION*2 + "app_label = '%s'\n" % APP_NAME
 
 
