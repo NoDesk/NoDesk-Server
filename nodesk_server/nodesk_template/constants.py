d0f61fd604033c10637fbd14dd819daceb965e3c
Clément Schreiner <clement@mux.me>
2014-06-24 20:27:25 +0200

    Make pep8 happy.

--- nodesk_server/nodesk_template/constants.py
+++ nodesk_server/nodesk_template/constants.py
@@ -1,1 +1,2 @@
-INDENTATION = 4 * ' ' # 4 space used for the metaprogramming of the django model of the templates
+# 4 space used for the metaprogramming of the django model of the templates
+INDENTATION = 4 * ' '
--- nodesk_server/nodesk_template/exceptions.py
+++ nodesk_server/nodesk_template/exceptions.py
@@ -1,11 +1,14 @@
 
-class UnrecognizedFieldType(Exception) :
-    def __init__(self,field_type,field_name) :
+class UnrecognizedFieldType(Exception):
+    def __init__(self, field_type, field_name):
         self.name = field_name
         self.type = field_type
-    def __str__(self) :
-        return "%s : Unrecongnized type for the field %s" % (self.type,self.name)
 
-class SectionFieldInSectionField(Exception) :
-    def __str__(self) :
+    def __str__(self):
+        return "%s : Unrecognized type for field %s" % (self.type,
+                                                        self.name)
+
+
+class SectionFieldInSectionField(Exception):
+    def __str__(self):
         return "Cannot put a Section field inside a Section field"
