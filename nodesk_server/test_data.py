3603eab41731cae04a1dda550b94f2b49aebc5ec
Thomas Kuntz <thomaskuntz@sharedmemory.fr>
2014-08-30 17:52:25 +0200

    change script according to previous commit

--- nodesk_server/test_data.py
+++ nodesk_server/test_data.py
@@ -3,5 +3,3 @@ import nodesk_template.models
 from django.core.management import call_command
 
 model_manager.sync_model("./nodesk_template/template_yaml")
-reload(nodesk_template.models)
-call_command('syncdb', interactive=False)
